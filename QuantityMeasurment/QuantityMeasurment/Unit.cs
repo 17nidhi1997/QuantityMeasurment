027bd755a452f49b919dc36e0666c3835b4977d4
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-05-17 12:50:35 +0530

    [Srinidhi]Added: refactor the code to Single Responsibility principle

---
+++ QuantityMeasurment/QuantityMeasurment/Unit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantityMeasurment
+{
+    // Enum For Length Unit.
+    public enum Unit
+    {
+        Feet, Inch, Yard, Centimeter
+    }
+
+}
