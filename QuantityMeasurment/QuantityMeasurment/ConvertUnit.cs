7db95c6b3d69634428ac15eb3b336ee1d387a789
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-05-17 13:13:38 +0530

    [Srinidhi]Refactor:Convet and Compare volumen in liters and gallon

--- QuantityMeasurment/QuantityMeasurment/ConvertUnit.cs
+++ QuantityMeasurment/QuantityMeasurment/ConvertUnit.cs
@@ -6,32 +6,40 @@ namespace QuantityMeasurment
 { 
     class ConvertUnit
     {
+        //Constants
+        private static readonly double FEET_TO_INCH_CONVERSION = 12;
+        private static readonly double YARD_TO_INCH_CONVERSION = 36;
+        private static readonly double CENTIMETER_TO_INCH_CONVERSION = 2.54;
+        private static readonly double GALLON_TO_LITRE_CONVERSION = 3.78;
+        private static readonly double MILILITRE_TO_LITRE_CONVERSION = 1000;
+
+        // Function To Covert Given Objects Unit Value Into Inch Unit.
         public static double ConvertToInch(length objectName)
         {
             double value = objectName.value;
             try
             {
-                //If Else Block for checking the specified Unit and converting into Centimeter.
-                if (objectName.unit.Equals(length.Unit.Feet))
+                //If Else Block for checking the specified Unit and converting into Inch.
+                if (objectName.unit.Equals(Unit.Feet))
                 {
-                    value = objectName.value * 12;
+                    value = objectName.value * FEET_TO_INCH_CONVERSION;
                 }
-                else if (objectName.unit.Equals(length.Unit.Yard))
+                else if (objectName.unit.Equals(Unit.Yard))
                 {
-                    value = objectName.value * 36;
+                    value = objectName.value * YARD_TO_INCH_CONVERSION;
                 }
-                else if (objectName.unit.Equals(length.Unit.Centimeter))
+                else if (objectName.unit.Equals(Unit.Centimeter))
                 {
-                    value = Math.Round(objectName.value / 2.54);
+                    value = Math.Round(objectName.value / CENTIMETER_TO_INCH_CONVERSION);
                 }
             }
-            catch (Exception)
+            catch (QuantityMeasurementException)
             {
                 throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.INVALID_TYPE, "Invalid");
             }
             return value;
         }
-        //Add length to units
+        //Add the length
         public double AddLengths(length object1, length object2)
         {
             try
@@ -44,6 +52,28 @@ namespace QuantityMeasurment
             {
                 throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.INVALID_TYPE, "Invalid Type");
             }
+        }
+
+        // Function to Convert Volume Into Litre.
+        public static double ConvertToLitre(Volume objectName)
+        {
+            double value = objectName.value;
+            try
+            {
+                if (objectName.unit.Equals(Unit.Gallon))
+                {
+                    value = objectName.value * GALLON_TO_LITRE_CONVERSION;
+                }
+                if (objectName.unit.Equals(Unit.Mililitre))
+                {
+                    value = objectName.value / MILILITRE_TO_LITRE_CONVERSION;
+                }
+            }
+            catch (QuantityMeasurementException)
+            {
+                throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.INVALID_TYPE, "Invalid Type");
+            }
+            return value;
         }
     }
 }
