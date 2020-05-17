1331b26cf77f4d73347c29d36f90247ef67145ec
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-05-17 12:19:39 +0530

    [Srinidhi]Added: Compare length in feet and Yard to convert units(inch, feet and yard)

---
+++ QuantityMeasurment/QuantityMeasurment/ConvertUnit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantityMeasurment
+{ 
+    class ConvertUnit
+    {
+        public static double ConvertToInch(length objectName)
+        {
+            double value = objectName.value;
+            try
+            {
+                //If Else Block for checking the specified Unit and converting into Centimeter.
+                if (objectName.unit.Equals(length.Unit.Feet))
+                {
+                    value = objectName.value * 12;
+                }
+                else if (objectName.unit.Equals(length.Unit.Yard))
+                {
+                    value = objectName.value * 36;
+                }
+            }
+            catch (Exception)
+            {
+                throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.INVALID_TYPE, "Invalid");
+            }
+            return value;
+        }
+    }
+}
