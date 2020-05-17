d55f7517f3a6657db8dbd4a11b0e332ebc712985
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-05-17 13:55:24 +0530

    [Srinidhi]Refactor: convert and Add weight in grams

--- QuantityMeasurment/QuantityMeasurment/ConvertUnit.cs
+++ QuantityMeasurment/QuantityMeasurment/ConvertUnit.cs
@@ -10,8 +10,9 @@ namespace QuantityMeasurment
         private static readonly double FEET_TO_INCH_CONVERSION = 12;
         private static readonly double YARD_TO_INCH_CONVERSION = 36;
         private static readonly double CENTIMETER_TO_INCH_CONVERSION = 2.54;
-        private static readonly double GALLON_TO_LITRE_CONVERSION = 3.78;
+        private static readonly double GALLON_TO_LITRE_CONVERSION = 3.785;
         private static readonly double MILILITRE_TO_LITRE_CONVERSION = 1000;
+        private static readonly double GRAM_TO_KILOGRAM_CONVERSION = 1000;
 
         // Function To Covert Given Objects Unit Value Into Inch Unit.
         public static double ConvertToInch(length objectName)
@@ -90,5 +91,47 @@ namespace QuantityMeasurment
                 throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.INVALID_TYPE, "Invalid Type");
             }
         }
+
+        /// <summary>
+        /// Function to Convert Weights Into Kilograms.
+        /// </summary>
+        /// <param name="objectName"></param>
+        /// <returns></returns>
+        public static double ConvertToKiloGram(Weight objectName)
+        {
+            try
+            {
+                double value = objectName.value;
+                if (objectName.unit.Equals(Unit.Grams))
+                {
+                    value = objectName.value / GRAM_TO_KILOGRAM_CONVERSION;
+                }
+                if (objectName.unit.Equals(Unit.Tonne))
+                {
+                    value = objectName.value * GRAM_TO_KILOGRAM_CONVERSION;
+                }
+                return value;
+            }
+            catch (QuantityMeasurementException)
+            {
+                throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.INVALID_TYPE, "Invalid Type");
+            }
+        }
+
+        // Function For Adding Weights And Results into Kilograms.
+        public static double AddWeights(Weight object1, Weight object2)
+        {
+            try
+            {
+                double value1 = ConvertToKiloGram(object1);
+                double value2 = ConvertToKiloGram(object2);
+                return value1 + value2;
+            }
+            catch (QuantityMeasurementException)
+            {
+                throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.INVALID_TYPE, "Invalid Type");
+            }
+        }
     }
 }
+
