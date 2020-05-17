1c49fb5974a598bddb13672cdb1ab4d1c5c9f37e
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-05-17 13:53:31 +0530

    [Srinidhi]Added: Add weight in grams

---
+++ QuantityMeasurment/QuantityMeasurment/IWeight.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantityMeasurment
+{
+    interface IWeight
+    { 
+        // Abstract Function For Checking Equal Values Implemenattion.
+        public abstract bool IsEqual(Weight object1, Weight object2);
+    }
+}
