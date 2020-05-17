cac88b197d725687652aab7c9546785109dbdba9
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-05-17 12:06:08 +0530

    [Srinidhi]Added: Exception class for handel the exception

---
+++ QuantityMeasurment/QuantityMeasurment/QuantityMeasurementException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantityMeasurment
+{
+    public class QuantityMeasurementException : Exception
+    {
+        // Enum For Defining Custom Exceptions
+        public enum ExceptionType
+        {
+            INVALID_TYPE,
+            Null_Exception
+        }
+        readonly ExceptionType type;
+        public QuantityMeasurementException(QuantityMeasurementException.ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
