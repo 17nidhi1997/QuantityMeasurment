23d0678edefc0e0598de27d08238728d5cc5e83c
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-05-17 12:51:00 +0530

    [Srinidhi]Added: refactor the code to Single Responsibility principle

---
+++ QuantityMeasurment/QuantityMeasurment/ILength.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantityMeasurment
+{
+    interface ILength
+    {
+        // Abstract Function IsEqual.
+        public abstract bool IsEqual(length object1, length object2);
+    }
+}
