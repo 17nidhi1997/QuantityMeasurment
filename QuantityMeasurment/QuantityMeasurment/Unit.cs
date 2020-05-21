6cdf4a36eb3db80a14bdf6ee504a131c530fbd6a
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-05-21 13:07:00 +0530

    [Srinidhi]Refactor: Enum class to convert all the Units

--- QuantityMeasurment/QuantityMeasurment/Unit.cs
+++ QuantityMeasurment/QuantityMeasurment/Unit.cs
@@ -2,23 +2,51 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace QuantityMeasurment
+namespace QuantityMeasurement
 {
-    // Enum For Length Unit and volumn unit.
-    public enum Unit
+    // Enum For Quanitity measure unit
+    public class Unit
     {
-        Feet,
-        Inch, 
-        Yard, 
-        Centimeter,
-        Gallon, 
-        Litre,
-        Mililitre, 
-        Grams,
-        Tonne,
-        Fahrenheit,
-        KiloGram,
-        Celsius
+    public static readonly Unit Feet = new Unit(12.0);
+    public static readonly Unit Inch = new Unit(1.0);
+    public static readonly Unit Centimeter = new Unit(0.4);
+    public static readonly Unit Yard = new Unit(36.0);
+    public static readonly Unit Litre = new Unit(1.0);
+    public static readonly Unit Mililitre = new Unit(0.001);
+    public static readonly Unit Gallon = new Unit(3.78);
+    public static readonly Unit KiloGram = new Unit(1.0);
+    public static readonly Unit Grams = new Unit(0.001);
+    public static readonly Unit Tonne = new Unit(1000);
+    public static readonly Unit Fahrenheit = new Unit(1.0);
+    public static readonly Unit Celsius = new Unit(2.12);
+    public static IEnumerable<Unit> values
+    {
+        get
+        {
+            yield return Feet;
+            yield return Inch;
+            yield return Centimeter;
+            yield return Yard;
+            yield return Litre;
+            yield return Mililitre;
+            yield return Gallon;
+            yield return KiloGram;
+            yield return Grams;
+            yield return Tonne;
+            yield return Fahrenheit;
+            yield return Celsius;
+        }
     }
 
-}
+    private double ConversionFactory;
+    private Unit(double baseUnit)
+    {
+        this.ConversionFactory = baseUnit;
+    }
+    public double GetConvertedValue(double values)
+    {
+        return this.ConversionFactory * values;
+    }
+
+}}
+
