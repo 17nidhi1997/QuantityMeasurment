90d9a403c4c57d9b653b132ff5a34c662fbb988e
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-05-21 13:08:16 +0530

    [Srinidhi]Refactor: Exception class to handel the exception

--- QuantityMeasurment/QuantityMeasurment/QuantityMeasurementException.cs
+++ QuantityMeasurment/QuantityMeasurment/QuantityMeasurementException.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace QuantityMeasurment
+namespace QuantityMeasurement
 {
     public class QuantityMeasurementException : Exception
     {
-        // Enum For Defining Custom Exceptions
+      
         public enum ExceptionType
         {
             INVALID_TYPE,
             Null_Exception
         }
+
         readonly ExceptionType type;
+
+        
         public QuantityMeasurementException(QuantityMeasurementException.ExceptionType type, string message) : base(message)
         {
             this.type = type;
