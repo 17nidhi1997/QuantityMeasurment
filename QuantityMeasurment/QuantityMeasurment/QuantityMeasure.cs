5aae73b4d109a34049564276ee7a707bff398821
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-05-27 12:16:50 +0530

    [Srinidhi]Refactor: Changes in commenting part

--- QuantityMeasurment/QuantityMeasurment/QuantityMeasure.cs
+++ QuantityMeasurment/QuantityMeasurment/QuantityMeasure.cs
@@ -6,32 +6,47 @@ namespace QuantityMeasurement
 {
     public class QuantityMeasure
     {
-        //Variables.
+        /// <summary>
+        /// Variables
+        /// </summary>
         public Unit unit;
         public double value;
 
+        /// <summary>
+        /// Default Constructor 
+        /// </summary>
         public QuantityMeasure()
         { }
-        // Parameter Constructor To Set Unit And Value.        
+
+        /// <summary>
+        /// Parameter Constructor To Set Unit And Value. 
+        /// </summary>
+        /// <param name="unit"></param>
+        /// <param name="value"></param>      
         public QuantityMeasure(Unit unit, double value)
         {
             this.unit = unit;
             this.value = value;
         }
-        /// Overriding Equals Function.        
+
+        /// <summary>
+        /// Overriding Equals Function.  
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>      
         public override bool Equals(object obj)
         {
-            //If Object is Null It Will Return exception.
+            ////If Object is Null It Will Return exception.
             if (obj == null)
             {
                 throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.Null_Exception, "Null");
             }
-            //If Object is Not A Reference Of Length Class The It Will Return False.
+           ////If Object is Not A Reference Of Length Class The It Will Return False.
             if (!(obj is QuantityMeasure))
             {
                 return false;
             }
-            //If Object is Not equal to type of the class then return exception.
+            ////If Object is Not equal to type of the class then return exception.
             if (this.GetType()!=obj.GetType())
             {
                 throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.INVALID_TYPE, "");
